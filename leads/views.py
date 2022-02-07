40b73ff7e9c1b3c7d69ca5dafd78fd8434356265
PRO-lchikoore <98795388+PRO-lchikoore@users.noreply.github.com>
2022-02-07 23:24:07 +0200

    first commit

---
+++ leads/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
+
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context={
+        "leads":leads
+    }
+    return render(request,"leads/leads_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead":lead
+    }
+    return render(request, "leads/lead_deatails.html",context)
